f612be053b460cb509ba435b7868d976621f5dd1
Simran Shaikh <simmoshaikh20.2003@gmail.com>
2024-12-30 21:56:15 +0530

    final commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Q-and-A-ChatBot
-QueryMaster: AI-Powered Q&amp;A Bot
--- app.py
+++ app.py
@@ -1,40 +1,80 @@
-## Conversational Q&A Chatbot
 import streamlit as st
-
-from langchain.schema import HumanMessage,SystemMessage,AIMessage
-from langchain.chat_models import ChatOpenAI
-
-## Streamlit UI
-st.set_page_config(page_title="Conversational Q&A Chatbot")
-st.header("Hey, Let's Chat")
-
-from dotenv import load_dotenv
-load_dotenv()
 import os
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-chat=ChatOpenAI(temperature=0.5)
+## load the GROQ And OpenAI API KEY 
+groq_api_key=os.getenv('GROQ_API_KEY')
+os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
 
-if 'flowmessages' not in st.session_state:
-    st.session_state['flowmessages']=[
-        SystemMessage(content="Yor are a comedian AI assitant")
-    ]
+st.title("Interview Preparation Q&A Chatbot")
 
-## Function to load OpenAI model and get respones
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="Llama3-8b-8192")
 
-def get_chatmodel_response(question):
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
 
-    st.session_state['flowmessages'].append(HumanMessage(content=question))
-    answer=chat(st.session_state['flowmessages'])
-    st.session_state['flowmessages'].append(AIMessage(content=answer.content))
-    return answer.content
+"""
+)
 
-input=st.text_input("Input: ",key="input")
-response=get_chatmodel_response(input)
+def vector_embedding():
 
-submit=st.button("Ask the question")
+    if "vectors" not in st.session_state:
 
-## If ask button is clicked
+        st.session_state.embeddings=GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+        st.session_state.loader=PyPDFDirectoryLoader("./study_material") ## Data Ingestion
+        st.session_state.docs=st.session_state.loader.load() ## Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200) ## Chunk Creation
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:20]) #splitting
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings) #vector OpenAI embeddings
 
-if submit:
-    st.subheader("The Response is")
-    st.write(response)
+
+
+
+
+prompt1=st.text_input("Enter Your Question for Topics(DBMS,HR,OOPS,OS,SQL)")
+
+
+if st.button("Documents Embedding"):
+    vector_embedding()
+    st.write("Vector Store DB Is Ready")
+
+import time
+
+
+
+if prompt1:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':prompt1})
+    print("Response time :",time.process_time()-start)
+    st.write(response['answer'])
+
+    # With a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # Find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
+
+
+
+
